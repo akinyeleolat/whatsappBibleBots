8d40fa453a444b1f1df48f6624f45082b382db53
System <none>
2017-04-21 21:44:04 +0000

    Checkpoint

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/status_callback", function (request, response) {
+  console.log('Request received: status ' + request.payload)
+  response.sendStatus(200);
+});
+
+app.get("/inbound_callback", function (request, response) {
+  console.log('Request received: inbound' + request.payload);
+  response.sendStatus(200);
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
