3279ff3b2b42ac0a5519b6d88f8cc4f0a21e7e31
System <none>
2017-04-28 19:22:36 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-// server.js
-// where your node app starts
-
 // init project
 var express = require('express')
   , bodyParser = require('body-parser');
 
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-//app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.post("/status_callback", function (request, response) {
-  console.log('Request received: status ');
+  console.log('Status received: ' + request.body.payload.message_id + ' is now ' + request.body.payload.message_status);
   response.sendStatus(200);
 });
 
@@ -27,12 +17,6 @@ app.post("/inbound_callback", function (request, response) {
   response.sendStatus(200);
 });
 
-app.get("/test", function(req, res) {
-  console.log("Hello!");
-  res.sendStatus(200);
-})
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
