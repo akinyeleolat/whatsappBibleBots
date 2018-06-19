c874c467ee9f4dfb764ca679b95d9c42b11ce80e
Glitch (whatsapp-webhooks) <none>
2018-06-19 03:40:39 +0000

    🌋🎈 Checkpoint ./package.json:322943/2069 ./server.js:322943/2793 ./public/client.js:322943/1928 ./views/index.html:322943/3416

--- server.js
+++ server.js
@@ -6,40 +6,14 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
+app.get("/", function (request, response) {
+  response.send('Simple WhatsApp Webhook tester</br>There is not front-end, see server.js for implementation');
+});
+
 app.post("/webhook", function (request, response) {
-  console.log(request.body);
+  console.log('Incoming Request:\n' + JSON.stringify(request.body, null, 2));
   response.sendStatus(200);
 });
-app.post("/status_callback", function (request, response) {
-  console.log('Status received: ' + request.body.payload.message_id + ' is now ' + request.body.payload.message_status);
-  response.sendStatus(200);
-});
-
-app.post("/inbound_callback", function (request, response) {
-  console.log('Message received: ' + JSON.stringify(request.body.payload));
-  response.sendStatus(200);
-});
-
-app.post("/media_callback", function (request, response) {
-  console.log('Media callback: ' + JSON.stringify(request.body.payload));
-  response.sendStatus(200);
-});
-
-app.post("/docker_email", function (request, response) {
-  console.log('Docker email POST: ' + JSON.stringify(request.body));
-
-  response.sendStatus(200);
-});
-
-app.get("/docker_email", function (request, response) {
-  console.log('Docker email GET: ' + JSON.stringify('OK'));
-  response.send('OK!');
-});
-
-app.get("/image", function (request, response) {
-  response.sendFile('/usr/local/wamedia/incoming/photos/WhatsApp/2017-04/IMG-20170428-WA003.jpg');
-});
-
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
