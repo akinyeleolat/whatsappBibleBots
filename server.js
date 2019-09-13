fc8840656387bec755044fc4ac10c43388e66c7b
Glitch (smoggy-soccer) <none>
2019-09-13 17:53:51 +0000

    🌓💆 Checkpoint ./package.json:6718622/981 ./server.js:6718622/1633

--- server.js
+++ server.js
@@ -2,14 +2,32 @@ var express = require('express')
   , bodyParser = require('body-parser');
 
 // var getJson = require('get-json');
+var axios = require('axios');
 
 var app = express();
+
+var instance = axios.create({
+  baseURL: 'https://bible-api.com/',
+  timeout: 1000,
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
 app.get("/", function (request, response) {
   response.send('Simple WhatsApp Webhook tester</br>There is no front-end, see server.js for implementation!');
+});
+
+app.get("/search/:query", function (request, response) {
+  console.log(request.params.query)
+//   async function getScripture() {
+//   try {
+//     const response = await instance.get('/user?ID=12345');
+//     response.send(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 });
 
 app.post("/webhook", function (request, response) {
